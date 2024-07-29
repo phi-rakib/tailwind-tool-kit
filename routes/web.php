d41d91ce560a1521947f46920346857ea814b4ea
rakib <phi.rakib@gmail.com>
2024-07-29 22:01:29 +0600

    added simple search

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('products', ProductController::class);
+Route::get('form/search', function () {
+    return view('forms.search');
+})->name('forms.search');
