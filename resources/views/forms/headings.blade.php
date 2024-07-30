09dc7edf469734d50f7c1945ea30df066dfcb751
rakib <phi.rakib@gmail.com>
2024-07-30 11:08:04 +0600

    added heading one to six

---
+++ resources/views/forms/headings.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('title', 'Alert')
+
+@section('content')
+<h1 class="mb-4 text-6xl font-bold leading-none tracking-tight">Heading 1</h1>
+<h2 class="mb-4 text-5xl font-bold leading-none tracking-tight">Heading 2</h2>
+<h3 class="mb-4 text-4xl font-bold leading-none tracking-tight">Heading 3</h3>
+<h4 class="mb-4 text-3xl font-bold leading-none tracking-tight">Heading 4</h4>
+<h5 class="mb-4 text-2xl font-bold leading-none tracking-tight">Heading 5</h5>
+<h6 class="mb-4 text-xl font-bold leading-none tracking-tight">Heading 6</h6>  
+@endsection
